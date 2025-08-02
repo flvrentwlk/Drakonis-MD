bc58ed6f088c5b67a52389e9a2ffbec3a915aa33
flvrentwlk <aborder.27-quai@icloud.com>
2025-08-02 20:58:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
-  OWNER_NUMBER: "1(236)362-1958", // PUT OWNER NUMBER HERE
+  SESSION_ID: "drakonis~0utAnY5D#wkaNr6MeGdwemthIgGsNKhPARIiZclsj7E2ELlmXiyg ",  // add your Session Id make sure it starts with 
+  OWNER_NUMBER: "1(236)362-1958", // +241 02866769
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
